5ef150f332490c2b6552152a22a4d3104b3d5211
Norman Wilson <wilsonnorman@yahoo.com>
2017-02-20 16:18:27 -0500

    commited changes to main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 /**
  * Created by DeMarcj on 2/20/2017.
  */
-public class Main {
+public class main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
