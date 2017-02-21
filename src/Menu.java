6c7c5f9a1f9bfcb12e764474cddf5055151aeceb
Charlie <CharlieThang@gmail.com>
2017-02-21 15:54:18 -0500

    second revision charlie

--- src/Menu.java
+++ src/Menu.java
@@ -60,20 +60,32 @@
         this.price = price;
     }
 
-    Menu food = new Menu("pizza", "Itlian", "osidjdflj", 54.00);
+    Menu food1 = new Menu("surfTurf", "Entree", "Dry aged 16oz. PorterHouse W/Veggies Grilled Shrimp", 54.00);
+    Menu food2 = new Menu("deMarcsLambCo", "Entree", "Lamb Chops, Baked Potato & Veggies", 45.00);
+    Menu food3 = new Menu("charlieSouthernSoul", "Entree", "Signature Greens, Baked Chicken", 48.00);
+    Menu food4 = new Menu("westernPeel", "Entree", "Barbecue Brisket, Broasted Potatoes W/Garlic Sauce & Cole Slaw",42.00);
+    Menu food5 = new Menu("asianWok", "Entree", "Asian Inspired Duck Breasts With Ginger Chili Glaze, Noodles & Mixed Veggies", 47.00);
+    Menu food6 = new Menu("normsCajunSkillet", "Entree", "Alligator Tenders, Dirty Rice & Gumbo", 54.00);
+    Menu food7 = new Menu("mexicanBowl", "Entree", "Steak, Chicken, Lime/Cilantro Rice, Refried Beans", 54.00);
+    Menu food8 = new Menu("deNorLiVeganSoul", "Entree", "Meatloaf W/Cauliflower & Grilled Asparagus", 30.00);
+    Menu food9 = new Menu("seaFoodJack", "Entree", "Shrimp & Grits, Crawfish Boil W/Veggies", 60.00);
+    Menu food10 = new Menu("WinBuckDepo", "Entree", "Bucket Of Wings, Potato Wedges, Cole Slaw", 30.50);
+    Menu food11 = new Menu("roastBeefParle", "Entree", "Roast Beef W/Roasted Potatoes & Carrots, Haricots Verts", 40.00);
+    Menu food12 = new Menu("burger360", "Entree", "16 oz. All Beef Burger (Choice Of Cheese) & Egg & With Onion Fries, Side Of Truffle Fries", 40.00);
 
-        // American Cusine Menu Items With Prices
+
+        // Authentic American Cuisine Menu Items With Prices
         /*
         double surfTurf = 34.50;
-        double lambCo = 34.50;
-        double southernSoul = 34.50;
+        double deMarcsLambCo = 34.50;
+        double charlieSouthernSoul = 34.50;
 
         double westernPeel = 26.50;
         double asianWok = 26.25;
-        double cajunSkillet = 26.25;
+        double normsCajunSkillet = 26.25;
 
         double mexicanBowl = 24.50;
-        double veganSoul = 24.50;
+        double deNorLiVeganSoul = 24.50;
         double seaFoodJack = 24.50;
 
         double winBuckDepo = 22.50;
@@ -83,20 +95,20 @@
 
         //int decide;
 
-        /*public MenuItems(double surfTurf, double lambCo, double southernSoul, double westernPeel,
-                         double asianWok, double cajunSkillet, double mexicanBowl, double veganSoul, double seaFoodJack,
+        /*public MenuItems(double surfTurf, double deMarcslambCo, double charlieSouthernSoul, double westernPeel,
+                         double asianWok, double normsCajunSkillet, double mexicanBowl, double deNorLiVeganSoul, double seaFoodJack,
                          double winBuckDepo, double roastBeefParle, double burger360) {
 
 
             this.total = total;
             this.surfTurf = surfTurf;
-            this.lambCo = lambCo;
-            this.southernSoul = southernSoul;
+            this.deMarcslambCo = deMarcslambCo;
+            this.charlieSouthernSoul = charlieSouthernSoul;
             this.westernPeel = westernPeel;
             this.asianWok = asianWok;
-            this.cajunSkillet = cajunSkillet;
+            this.normsCajunSkillet = normsCajunSkillet;
             this.mexicanBowl = mexicanBowl;
-            this.veganSoul = veganSoul;
+            this.deNorLiVeganSoul = deNorLiVeganSoul;
             this.seaFoodJack = seaFoodJack;
             this.winBuckDepo = winBuckDepo;
             this.roastBeefParle = roastBeefParle;
